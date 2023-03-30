6ddff1ef6f5e21a6b88cd5b07de24115d9b5e0c7
luca-breebaart <114395762+luca-breebaart@users.noreply.github.com>
2023-03-30 10:20:39 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
 import './App.css';
-import {Routes ,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Result from './Result';
+import Compare from './Compare';
+import Timeline from './Timeline';
 
 function App() {
+  
   return (
+
     <Routes>
-     <Route path='/' element= { <Home /> } />
-     <Route path='Result' element= { <Result /> } />
+      <Route path='/' element={<Home />} />
+      <Route path='Compare' element={<Compare />} />
+      <Route path='Timeline' element={<Timeline />} />
     </Routes>
 
   );
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import image from './headerimage.png';
 import carImage from './carimage.jpg';
 import { useEffect, useState } from "react";
 import './App.css'
+
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { NavLink } from 'react-router-dom';
+
 
 
 
@@ -20,9 +23,9 @@ const Home = () => {
             <div className="header">
 
                 <nav className="topnav">
-                    <NavLink exact to="/" activeClassName="active">Home</NavLink>
-                    <NavLink to="/compare" activeClassName="active">Compare</NavLink>
-                    <NavLink to="/timeline" activeClassName="active">Timeline</NavLink>
+                    <NavLink  to="/" activeclassname="active">Home</NavLink>
+                    <NavLink to="/Compare" activeclassname="active">Compare</NavLink>
+                    <NavLink to="/Timeline" activeclassname="active">Timeline</NavLink>
                 </nav>
 
 
@@ -40,34 +43,35 @@ const Home = () => {
             <div className="section-1">
 
 
-                <div class="car-search-container">
-                    <div class="car-search">
-                        <form>
-                            <div class="form-group">
-                                <label for="make">Make:</label>
-                                <select name="make" id="make">
-                                    <option value="">--Select Make--</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="model">Model:</label>
-                                <select name="model" id="model">
-                                    <option value="">--Select Model--</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="year">Year:</label>
-                                <select name="year" id="year">
-                                    <option value="">--Select Year--</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <button type="button" class="search-btn">Search</button>
-                            </div>
-                        </form>
+                <div className="d-flex align-items-center justify-content-center">
+                    <div className="card">
+                        <div className="card-body">
+                            <form className="row g-4 d-flex align-items-end">
+                                <div className="col-md-3">
+                                    <label htmlFor="make" className="form-label">Make:</label>
+                                    <select name="make" id="make" className="form-control">
+                                        <option value="">--Select Make--</option>
+                                    </select>
+                                </div>
+                                <div className="col-md-3">
+                                    <label htmlFor="model" className="form-label">Model:</label>
+                                    <select name="model" id="model" className="form-control">
+                                        <option value="">--Select Model--</option>
+                                    </select>
+                                </div>
+                                <div className="col-md-3">
+                                    <label htmlFor="year" className="form-label">Year:</label>
+                                    <select name="year" id="year" className="form-control">
+                                        <option value="">--Select Year--</option>
+                                    </select>
+                                </div>
+                                <div className="col-md-3">
+                                    <button type="button" className="btn btn-primary w-100">Search</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-
 
                 <div className="home-section">
                     <div className="container">
@@ -78,7 +82,7 @@ const Home = () => {
                             <div className="col-md-6 text">
                                 <h2>Find the Best Car for You</h2>
                                 <p>Search through our vast database of cars to find the one that suits your needs. We offer comprehensive information on cars, including specifications, features, and reviews from real customers. With our user-friendly platform, you can easily compare different cars and make an informed decision.</p>
-                                <a href="/compare" className="btn btn-primary">Compare Cars</a>
+                                <a href="/compare" className="btn btn-primary btn-lg">Compare Cars</a>
                             </div>
                         </div>
                     </div>
@@ -87,6 +91,9 @@ const Home = () => {
 
             </div>
         </div>
+
+
+
     )
 }
 
